8190dcde74c973045d20d36062a9f9fe49f4ada5
backyardwombat <mcneese.casey@gmail.com>
2017-02-13 08:04:23 -0800

    First version of the Plantbot custom BLE profile.

---
+++ PlantSensorCustomProfile.cydsn/ble_process.c
@@ -0,0 +1,44 @@
+/* ========================================
+ *
+ * Copyright Majellatack, 2017
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your Majellatech.
+ *
+ * ========================================
+*/
+#if !defined (BLEPROCESS_H)
+#define BLEPROCESS_H
+#include <project.h>
+    
+    
+/*******************************************************************************
+* Function Name: UpdateMoisturecharacteristic
+********************************************************************************
+* Summary:
+*        Update the GATT DB with new Moisture value received.
+*
+* Parameters:
+*  moistData:   array pointer where moisture value is stored
+*  len: 		length of the array
+*  attrHandle:	attribute handle of the characteristic
+*
+* Return:
+*  void
+*
+*******************************************************************************/
+void UpdateMoistureCharacteristic(uint8* moistData, uint8 len, uint16 attrHandle)
+{
+    CYBLE_GATT_HANDLE_VALUE_PAIR_T moistureHandle;
+    
+    moistureHandle.attrHandle = attrHandle;
+    moistureHandle.value.val = moistData;
+    moistureHandle.value.len = len;    
+    
+    //TODO, What should the 2nd argument be? 91162 has it set to FALSE which is defined as zero
+    CyBle_GattsWriteAttributeValue(&moistureHandle, 0x00, &cyBle_connHandle, CYBLE_GATT_DB_PEER_INITIATED);  
+}
+#endif
+/* [] END OF FILE */
