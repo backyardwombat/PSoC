dba086067c7661d5c90f4c42235e00eae0f5fe11
backyardwombat <mcneese.casey@gmail.com>
2017-11-19 13:02:20 -0800

    adding light sensor into Plant Sensor project

--- EnvironmentalTelemetry/PlantSensorCustomProfile.cydsn/ble_process.c
+++ EnvironmentalTelemetry/PlantSensorCustomProfile.cydsn/ble_process.c
@@ -13,7 +13,34 @@
 #define BLEPROCESS_H
 #include <project.h>
     
+ 
+/*******************************************************************************
+* Function Name: UpdateLightCharacteristic
+********************************************************************************
+* Summary:
+*        Update the GATT DB with new Light value received.
+*
+* Parameters:
+*  moistData:   array pointer where light value is stored
+*  len: 		length of the array
+*  attrHandle:	attribute handle of the characteristic
+*
+* Return:
+*  void
+*
+*******************************************************************************/
+void UpdateLightCharacteristic(uint8* lightData, uint8 len, uint16 attrHandle)
+{
+    CYBLE_GATT_HANDLE_VALUE_PAIR_T lightHandle;
     
+    lightHandle.attrHandle = attrHandle;
+    lightHandle.value.val = lightData;
+    lightHandle.value.len = len;    
+    
+    //TODO, What should the 2nd argument be? 91162 has it set to FALSE which is defined as zero
+    CyBle_GattsWriteAttributeValue(&lightHandle, 0x00, &cyBle_connHandle, CYBLE_GATT_DB_PEER_INITIATED);  
+}
+
 /*******************************************************************************
 * Function Name: UpdateMoisturecharacteristic
 ********************************************************************************
--- EnvironmentalTelemetry/PlantSensorCustomProfile.cydsn/main.c
+++ EnvironmentalTelemetry/PlantSensorCustomProfile.cydsn/main.c
@@ -11,6 +11,8 @@
 */
 #include "project.h"
 #include "ble_process.h"
+#include "common.h"
+#include <tsl2561_i2c_driver.h>
 
 /***************************************
 *           API Constants
@@ -110,11 +112,64 @@ void SpoofMoisture(void)
     }
 }
 
+uint32 _WaitForWriteComplete(void)
+{
+    uint32 status;
+    uint32 timeout = I2C_TIMEOUT_INTERVAL;
+    char statusMessage[5];
+
+    /* Waits until master completes write transfer */
+    while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_WR_CMPLT) && (0u != timeout))
+    {
+        #if (DEBUG_UART_ENABLED == ENABLED)
+            if(0u == (timeout % DEBUG_WAIT_UPDATE_INTERVAL))
+            {
+                UART_DEB_UartPutString("Waiting for Write Complete \r\n");
+            }
+        #endif
+        timeout--;
+    }
+    status = I2C_I2CMasterStatus();
+    sprintf(statusMessage, "%08lx", status);
+    if(I2C_I2C_MSTAT_WR_CMPLT == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_WR_CMPLT))
+    {
+        /* Write complete */
+        #if (DEBUG_UART_ENABLED == ENABLED)
+        UART_DEB_UartPutString("Write Complete, status = "); 
+        #endif
+    }
+    else
+    {
+        /* I2C timeout */
+        #if (DEBUG_UART_ENABLED == ENABLED)
+        UART_DEB_UartPutString("I2C Timeout, status = ");
+        #endif
+    }
+
+    #if (DEBUG_UART_ENABLED == ENABLED)
+    UART_DEB_UartPutString(statusMessage);
+    UART_DEB_UartPutString("\n\r");
+    #endif
+    
+    I2C_I2CMasterClearStatus();
+    return status;
+}
+
+
+union TwoBytes
+{
+    uint16 u16;
+    uint8 u8[2];
+};
+
 int main(void)
 {
     uint8 moist_array[MOISTURE_DATA_CHAR_LEN];
-    
+    uint32 i2cStatus;
     uint8 battery = 0x69;
+    uint8 data_block[5];
+    uint8 ill_result_zero[2];
+    uint8 ill_result_one[2];
     
     CYBLE_GATT_HANDLE_VALUE_PAIR_T battHandle;
     battHandle.attrHandle = CYBLE_PLANTBOT_BATTERY_LEVEL_CHAR_HANDLE;
@@ -123,6 +178,29 @@ int main(void)
     
     CyGlobalIntEnable; /* Enable global interrupts. */
 
+    #if (DEBUG_UART_ENABLED == ENABLED)
+	    UART_DEB_Start();
+    #endif /* (DEBUG_UART_ENABLED == ENABLED) */
+    
+    I2C_Start();
+    I2C_EnableInt(); 
+    
+        /******************* Disable the TSL2561 ************************/
+    #if (DEBUG_UART_ENABLED == ENABLED)
+    UART_DEB_UartPutString("Disabling Sensor \r\n");
+    #endif
+    i2cStatus = TSL2561_WriteByte(ADDRESS_FLOAT, (COMMAND_CMD_MSK | CONTROL_REG), CONTROL_POWER_OFF);
+    i2cStatus = _WaitForWriteComplete();
+    
+    CyDelay(50);
+    
+    /******************* Enable the TSL2561 *************************/
+    #if (DEBUG_UART_ENABLED == ENABLED)
+    UART_DEB_UartPutString("Enabling Sensor \r\n");
+    #endif
+    i2cStatus = TSL2561_WriteByte(ADDRESS_FLOAT, (COMMAND_CMD_MSK | CONTROL_REG), CONTROL_POWER_ON);
+    i2cStatus = _WaitForWriteComplete();
+    
     /* Using the same LED format as the CY code examples */
     Disconnect_LED_Write(LED_OFF);
     Advertising_LED_Write(LED_OFF);
@@ -134,10 +212,32 @@ int main(void)
     {
         CyBle_GattsWriteAttributeValue(&battHandle, 0x00, &cyBle_connHandle, CYBLE_GATT_DB_PEER_INITIATED); 
         
+        /* Moisture Measurement */
         SpoofMoisture();
         moist_array[0] = (uint8) (moisture & 0x00FF);
         moist_array[1] = (uint8) ((moisture & 0xFF00) >> 8);
         
+        /* Light Measurement */
+                /************ READ BLOCK ***********************/
+        TSL2561_SendCmd(ADDRESS_FLOAT, COMMAND_READ_BLOCK);
+        
+        /* Waits until master completes write transfer */
+        while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_WR_CMPLT));
+        i2cStatus = I2C_I2CMasterStatus();
+        I2C_I2CMasterClearStatus();
+        
+        TSL2561_ReadData(ADDRESS_FLOAT, BLOCK_SIZE, data_block);
+        
+        /* Wait until master completes read */
+        while (0u == (I2C_I2CMasterStatus() & I2C_I2C_MSTAT_RD_CMPLT));
+        i2cStatus = I2C_I2CMasterStatus();
+        
+        ill_result_zero[0] = data_block[1];
+        ill_result_zero[1] = (data_block[2] << 8);
+        ill_result_one[0] = data_block[3];
+        ill_result_one[1] = (data_block[4] << 8);        
+        
+        UpdateMoistureCharacteristic(ill_result_zero, sizeof(ill_result_zero),CYBLE_PLANTBOT_ILLUMINANCE_CHAR_HANDLE);
         UpdateMoistureCharacteristic(moist_array, sizeof(moist_array),CYBLE_PLANTBOT_SOIL_MOISTURE_CHAR_HANDLE);
         
         HandleLeds();
